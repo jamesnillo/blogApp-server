05d78bbe55b0bcfe93f97c2d02e7b1a0e7d0ba9b
James Sergio Nillo <james.nillo10@gmail.com>
2024-12-17 14:46:41 +0800

    Added activity code for s86

---
+++ routes/blog.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const blogController = require("../controllers/blog");
+const { verify, verifyAdmin } = require("../auth.js");
+
+const router = express.Router();
+
+router.post("/", verify, blogController.createBlog);
+
+router.get('/getMyBlogs', verify, blogController.getMyBlog);
+
+router.get("/all", blogController.getAllBlogs);
+
+router.get("/specific/:id", blogController.getBlog);
+
+router.patch("/:blogId", verify, blogController.updateBlog);
+
+router.delete("/:blogId", verify, blogController.deleteBlog);
+
+
+
+
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userController = require('../controllers/user.js');
+const { verify, verifyAdmin } = require("../auth.js");
+
+const router = express.Router();
+
+router.post("/register", userController.registerUser);
+
+router.post("/login", userController.loginUser);
+
+router.get("/details", verify, userController.getProfile);
+
+
+
+
+
+
+
+
+module.exports = router;
