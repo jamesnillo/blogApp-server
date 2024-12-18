21db0a751bdecb14fa107826975f2e73ac7852d6
James Sergio Nillo <james.nillo10@gmail.com>
2024-12-18 10:29:12 +0800

    Updated repo

--- index.js
+++ index.js
@@ -20,12 +20,8 @@ db.once("open", ()=> console.log("Now connected to MongoDB Atlas."));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const corsOptions = {
-	origin: ['http://localhost:3000', 'https://blog-app-client-mauve.vercel.app/'],
-	optionsSuccessStatus: 200
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use("/users", userRoutes);
