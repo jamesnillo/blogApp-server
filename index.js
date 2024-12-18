0e79d9688aed0be8c294b6ff057dfaec1628925c
James Sergio Nillo <james.nillo10@gmail.com>
2024-12-18 10:13:33 +0800

    Updated repo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-	origin: ['http://localhost:3000'],
+	origin: ['http://localhost:3000', 'https://blog-app-client-mauve.vercel.app/'],
 	optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
